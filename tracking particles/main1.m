2d667e74033d7b146994db6047a2f2a7f7b45fa2
slowlymiao <55442995+slowlymiao@users.noreply.github.com>
2019-12-04 11:40:13 +0800

    Add files via upload

--- tracking particles/main1.m
+++ tracking particles/main1.m
@@ -16,20 +16,15 @@ Q=[0.3,3];
 R=[0.2,2];
 P=[0.3,3];
 %--------------file information----------------------------------------------------
-StartFile=1;%第一帧结果已知
+StartFile=1;
 EndFile=22;
 FileStep=1;
-IPath='C:\Users\PIVGroup\Desktop\image\';
+IPath='E:\科研\博后工作\小论文\颗粒追踪方法\data&code\matlabcode\公开代码\image\';
 maxn=0;
 
 for nFile=StartFile:FileStep:EndFile-1
     
-    if nFile>StartFile && points(nFile-FileStep)~=0
-        fcentt=centi(:,1:2)+dis(:,1:2);
-        fcentt(:,3:5)=centi(:,3:5);
-        fdis=dis;
-        clear centt dis centi centt
-    end
+    
     %----------Pre-processing----------------------------------
     filename1=[IPath,num2str(nFile),'.mat'];
     filename2=[IPath,num2str(nFile+FileStep),'.mat'];
@@ -40,15 +35,16 @@ for nFile=StartFile:FileStep:EndFile-1
     
     
     %---------Two subtraction------------------------------------------
-    [cent1,fc]=Two_Subtraction(Image1,Image2,rad,5);
+    [cent1]=Two_Subtraction(Image1,Image2,rad,5);
     cent2=[];
     if isempty(cent1)==0
-        [cent2,fc1]=FindCent(Image1,cent1,fc,rad,rangec);
-        if isempty(cent2)==0
-            [cent2,~]=Delete_SimilarP(cent2,ceil(1.0*rad),fc1);
+        [cent2]=FindCent(Image1,cent1,rad,rangec);
+        if size(cent2,1)>1
+            [a,~]=Delete_SimilarP(cent2,cent2,rad,1);
+            cent2(a,:)=[];
         end
     end
-    clear fc fc1 cent1
+    clear cent1 a 
     %-------initialization---------------------------------------------------------
     if isempty(cent2)==0
         fp=zeros(length(cent2(:,1)),5);
@@ -57,9 +53,10 @@ for nFile=StartFile:FileStep:EndFile-1
         cent2(:,4:5)=0;
         %------------Associate with previous frame-------------------------------------
         if nFile>StartFile && points(nFile-FileStep)~=0
-            m1=fcentt';
-            m2=cent2(:,1:3)';
-            [rp1,rp2]=Data_Association(m1,m2,2*rad);
+            fcentt=[centt(:,1:2),centi(:,3:5)];
+            fdis=dis;
+            clear dis centi centt
+            [rp1,rp2]=Delete_SimilarP(fcentt(:,1:2)',cent2(:,1:2)',2*rad,2);
             if isempty(rp1)==0 %%存在关联值
                 atemp=tabulate(rp1);
                 btemp=find(atemp(:,2)>1);
@@ -77,7 +74,6 @@ for nFile=StartFile:FileStep:EndFile-1
                         temp=find(rp1==atemp(ctemp(ii)));
                         fcentt(rp1(temp),1:3)=round(0.5*cent2(rp2(temp),1:3)+0.5*fcentt(rp1(temp),1:3));
                         clear temp dd
-                        %                         end
                     end
                 end
                 clear atemp btemp ctemp
@@ -101,50 +97,32 @@ for nFile=StartFile:FileStep:EndFile-1
         end
     end
     
-    points(nFile)=0;
-    centi=zeros(1,5);
-    dis=zeros(1,3);
-    centt=zeros(1,2);
-    
-    
     if isempty(cent)==0
         %-------------cross-correlation------------------------------------------
-        [s1,dis,fp1]=Cro_correlation(cent',Image1,Image2,fp,th,Q,R,rad);%centi为初始点位置标志，dis为位移；采用原始图片
-        
-        
-        if sum(sum(s1))>0
-            btemp=find(s1(1,:)==0);
-            centi=s1(:,s1(1,:)~=0)';
-            atemp=find(centi(:,5)==0);
-            centi(atemp,5)=maxn+1:maxn+length(atemp);
-            centi(:,4)=centi(:,4)+1;
-            dis(:,btemp)=[];
-            dis=dis';
-            fp1(btemp,:)=[];
-            fp1(:,3)=dis(:,3);
-            
-            [~,a,b]=Auto_Association(centt',rad);
-            if isempty(a)==0
-                m1=[];
-                for aa=1:length(a)
-                    if centi(a(aa),4)>centi(b(aa),4)
-                        m1=[m1,b];
-                    elseif centi(a(aa),4)<centi(b(aa),4)
-                        m1=[m1,a];
-                    else
-                        m1=[m1,b];
-                    end
+        [centi,dis,fp1]=Cro_correlation(cent',Image1,Image2,fp,th,Q,R,rad);
+        atemp=find(centi(:,5)==0);
+        centi(atemp,5)=maxn+1:maxn+length(atemp);
+        centi(:,4)=centi(:,4)+1;
+        centt=centi(:,1:2)+dis(:,1:2);
+        [a,~]=Delete_SimilarP(centt',centt',rad,1);
+        if isempty(a)==0
+            m1=[];
+            for aa=1:length(a)
+                if centi(a(aa),4)>centi(b(aa),4)
+                    m1=[m1,b];
+                elseif centi(a(aa),4)<centi(b(aa),4)
+                    m1=[m1,a];
+                else
+                    m1=[m1,b];
                 end
-                
-                centi(m1,:)=[];
-                dis(m1,:)=[];
-                fp1(m1,:)=[];
-                centt(m1,:)=[];
             end
-            points(nFile)=size(dis,1);
-            maxn=maxn+length(atemp);
-            clear atemp btemp a b cent
+            centi(m1,:)=[];
+            dis(m1,:)=[];
         end
+        points(nFile)=size(dis,1);
+        maxn=maxn+length(atemp);
+        clear atemp  a b cent 
+        
     end
     
     Result1(nFile,1)=nFile;
@@ -152,9 +130,10 @@ for nFile=StartFile:FileStep:EndFile-1
     Result2(sum(points(1:nFile))-points(nFile)+1:sum(points(1:nFile)),1)=nFile;
     Result2(sum(points(1:nFile))-points(nFile)+1:sum(points(1:nFile)),2)=1:points(nFile);
     Result2(sum(points(1:nFile))-points(nFile)+1:sum(points(1:nFile)),3:8)=[centi(:,1:2),dis(:,1:2),centi(:,3),centi(:,5)];
-
+    clear filename1 filename2 Image1 Image2 fp
 end
- %%%%%%%%%step3%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clearvars -EXCEPT Result1 Result2
+%%%%%%%%%step3%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 th1=2;
 th2=10;
 ss=1;
@@ -169,9 +148,11 @@ mu=mean(c1(:,6)./c1(:,2));%平均位移
 if isempty(find(c11(:,5)>0))==0
     [nn3,p3]=Repair_BC(c11,Result2,Result1(:,1),Result1(:,2),d1,ss);
     [c3,~,~]=Statistic_Chain(nn3);
-    [c4,~,~,p4,nn4]=Delete_Shortchain(c3,d1,av1,n1,p3,nn3,rad1,th);
 end
-
+Result=c3;
+Result1=p3;
+Result2=nn3;
+clearvars -EXCEPT Result1 Result2 Result
 
 
 
